c8689089e1829a3b136a94ca2c5da1a129a7c521
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2023-01-14 12:16:20 +0530

    Fixed bugs and added environment variables

--- corpus/corpus/settings.py
+++ corpus/corpus/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ["DEBUG"] == "True"
 
 ALLOWED_HOSTS = ["*"]
 
