afca5d26176d1c467f6f549800aec04cd48df3d8
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2023-07-07 07:57:47 +0530

    Fixed some URLs

--- backend/start.sh
+++ backend/start.sh
@@ -2,13 +2,12 @@
 
 echo "started setup"
 
-echo "checking database connectivity"
-python check_database.py
-echo "database connected...continuining..."
-
 # Setup database
 python manage.py makemigrations
 python manage.py migrate --no-input
+
+# Collect static files
+python manage.py collectstatic --no-input
 
 # Start server
 gunicorn --bind 0.0.0.0:$DJANGO_BACKEND_PORT --workers 4 corpus.wsgi
