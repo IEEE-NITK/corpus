59308f20722202d1549ab88d2227e4e0d0cce2f0
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2023-07-07 07:21:49 +0530

    Removed Django static assets

--- backend/start.sh
+++ backend/start.sh
@@ -10,8 +10,5 @@ echo "database connected...continuining..."
 python manage.py makemigrations
 python manage.py migrate --no-input
 
-# Collect static files
-python manage.py collectstatic --no-input
-
 # Start server
 gunicorn --bind 0.0.0.0:$DJANGO_BACKEND_PORT --workers 4 corpus.wsgi
