938bd47d82392e2c7822d293fe10d86b2849d31b
Anirudh Prabhakaran <76864239+anirudhprabhakaran3@users.noreply.github.com>
2023-08-25 14:42:49 +0530

    Bring back to Django (#16)
    * Bring back to Django
    
    * Fix github tests
    
    * Ensure shell scripts always have LF line endings
    
    Signed-off-by: Nishant Nayak <nishantnayak2001@gmail.com>
    
    * Made a few more changes to docker files
    
    * Added CONTRIBUTING guidelines
    
    ---------
    
    Signed-off-by: Nishant Nayak <nishantnayak2001@gmail.com>
    Co-authored-by: Nishant Nayak <nishantnayak2001@gmail.com>

--- corpus/corpus/settings.py
+++ corpus/corpus/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -52,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "corpus.base_url"
+ROOT_URLCONF = "corpus.urls"
 
 TEMPLATES = [
     {
