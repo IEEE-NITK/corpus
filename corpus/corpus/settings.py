e4d94aee0c186e6411050d1b8965a02173def713
Nishant Nayak <66667161+nishant-nayak@users.noreply.github.com>
2023-09-17 10:04:17 +0530

    Update corpus/corpus/settings.py
    Update config notation to match standard
    
    Co-authored-by: Anirudh Prabhakaran <76864239+anirudhprabhakaran3@users.noreply.github.com>

--- corpus/corpus/settings.py
+++ corpus/corpus/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "pages.apps.PagesConfig",
-    "config",
+    "config.apps.ConfigConfig",
 ]
 
 MIDDLEWARE = [
