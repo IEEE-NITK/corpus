69d3e425fe2a411355a99380be1214d685c07b3d
Nishant Nayak <66667161+nishant-nayak@users.noreply.github.com>
2023-09-24 06:29:57 +0000

    Update password declaration
    Signed-off-by: GitHub <noreply@github.com>

--- corpus/start_livecycle.sh
+++ corpus/start_livecycle.sh
@@ -2,5 +2,5 @@
 
 python manage.py migrate --no-input
 python manage.py loaddata config_db.json
-python manage.py createsuperuser --no-input --username admin --email admin@example.com --password admin
+DJANGO_SUPERUSER_PASSWORD=admin python manage.py createsuperuser --no-input --username admin --email admin@example.com
 python manage.py runserver 0.0.0.0:8000
