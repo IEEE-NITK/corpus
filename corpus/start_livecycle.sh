11862a3dd20b11ce3fdbfcdd05c4474e9ac00a02
Nishant Nayak <66667161+nishant-nayak@users.noreply.github.com>
2023-09-24 06:24:23 +0000

    Add superuser creation in script
    Signed-off-by: GitHub <noreply@github.com>

--- corpus/start_livecycle.sh
+++ corpus/start_livecycle.sh
@@ -2,4 +2,5 @@
 
 python manage.py migrate --no-input
 python manage.py loaddata config_db.json
+python manage.py createsuperuser --no-input --username admin --email admin@example.com --password admin
 python manage.py runserver 0.0.0.0:8000
