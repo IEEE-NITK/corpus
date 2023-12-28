2cd2be6053b0378f2b55b3a859363d203ebdc3c2
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2023-12-28 12:39:28 +0530

    Expose ports on docker

--- corpus/corpus/settings.py
+++ corpus/corpus/settings.py
@@ -175,5 +175,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "corpusieeenitk@gmail.com")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "gmailapppassword")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
