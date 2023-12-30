c726647fdbb32a6b121091c582a04b85df327df2
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2023-12-30 10:33:52 +0530

    Add toolchain for dev docker compose

---
+++ corpus/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./templates/*.html",
+        "./templates/**/*.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/container-queries"),
+        require("daisyui"),
+    ],
+}
