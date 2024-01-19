59d1714d9e6d7a61cb08c292c5f5ea9dfd5942ce
Nishant Nayak <nishantnayak2001@gmail.com>
2024-01-19 21:49:24 +0530

    Add achievements models
    Signed-off-by: Nishant Nayak <nishantnayak2001@gmail.com>

---
+++ corpus/pages/models.py
@@ -0,0 +1,50 @@
+from accounts.models import User
+from django.db import models
+
+
+class Achievement(models.Model):
+    title = models.CharField(max_length=128, verbose_name="Title")
+    date = models.DateField(verbose_name="Date of Achievement")
+    url = models.URLField(blank=True, null=True, verbose_name="Related URL")
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Related User",
+        blank=True,
+        null=True,
+    )
+    description = models.TextField(verbose_name="Description")
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    TAGS = [
+        ("GSOC", "Google Summer of Code"),
+        ("IAS SRFP", "IAS Summer Research Fellowship Program"),
+        ("SB", "Student Branch Award"),
+        # TODO: Add more tags
+    ]
+
+    tag = models.CharField(max_length=32, choices=TAGS, verbose_name="Tag")
+    achievement = models.ForeignKey(
+        Achievement, on_delete=models.CASCADE, verbose_name="Achievement"
+    )
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=256, verbose_name="Title")
+    date = models.DateField(verbose_name="Publish Date")
+    url = models.URLField(verbose_name="URL")
+    authors = models.CharField(max_length=256, verbose_name="Authors")
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Related User",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.title
