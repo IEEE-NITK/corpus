19d5226a2388397b1f0382f125c81f8af948bbef
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2024-02-04 20:14:22 +0530

    Add checks for home buttons

--- corpus/skyward_expedition/models.py
+++ corpus/skyward_expedition/models.py
@@ -1,3 +1,41 @@
+from accounts.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class SEUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(
+        "Team", on_delete=models.CASCADE, related_name="team", blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.user.__str__()
+
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=200, blank=False, null=False)
+    team_leader = models.ForeignKey(
+        SEUser, on_delete=models.CASCADE, related_name="leader"
+    )
+
+    def __str__(self):
+        return self.team_name
+
+
+class Invite(models.Model):
+    inviting_team = models.ForeignKey(
+        Team, on_delete=models.CASCADE, related_name="invite_to_team"
+    )
+    invite_email = models.EmailField(blank=False, null=False)
+
+    def __str__(self):
+        return self.invite_email
+
+
+class Announcement(models.Model):
+    content = models.TextField(blank=False, null=False)
+    url_link = models.URLField(blank=True, null=True)
+    url_link_text = models.CharField(max_length=200, blank=True, null=True)
--- corpus/skyward_expedition/views.py
+++ corpus/skyward_expedition/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from skyward_expedition.models import SEUser
 
 
 # Create your views here.
 def home(request):
-    return render(request, "skyward_expedition/home.html")
+    args = {}
+
+    if request.user is not None and request.user.is_authenticated:
+        se_user = SEUser.objects.filter(user=request.user).first()
+        if se_user is not None:
+            args["dashboard"] = True
+
+        if request.user.groups.filter(name__in="se_admin").exists():
+            args["admin"] = True
+
+    return render(request, "skyward_expedition/home.html", args)
