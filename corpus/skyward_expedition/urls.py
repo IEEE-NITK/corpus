45459df659d590bba3a8c18cb12dc5480fe20493
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2024-02-13 22:04:22 +0530

    Add team registrations

--- corpus/skyward_expedition/urls.py
+++ corpus/skyward_expedition/urls.py
@@ -5,4 +5,18 @@ urlpatterns = [
     path("", views.home, name="skyward_expedition_home"),
     path("register/", views.register, name="skyward_expedition_register"),
     path("dashboard/", views.dashboard, name="skyward_expedition_dashboard"),
+    path("create_team/", views.create_team, name="skyward_expedition_create_team"),
+    path(
+        "create_invite/", views.create_invite, name="skyward_expedition_create_invite"
+    ),
+    path(
+        "accept_invite/<int:pk>/",
+        views.accept_invite,
+        name="skyward_expedition_accept_invite",
+    ),
+    path(
+        "delete_invite/<int:pk>/",
+        views.delete_invite,
+        name="skyward_expedition_delete_invite",
+    ),
 ]
