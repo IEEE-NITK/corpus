dffa01b8a1f259ae87a11fb82f6999ac674b526a
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2024-02-19 21:26:43 +0530

    Add media upload capability

--- corpus/corpus/settings.py
+++ corpus/corpus/settings.py
@@ -149,10 +149,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "templates/static"]
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
-else:
-    MEDIA_ROOT = os.path.join(BASE_DIR, "templates/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
