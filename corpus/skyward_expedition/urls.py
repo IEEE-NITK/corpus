aa9976f047167e42caaf071782b47db64a113b51
Anirudh Prabhakaran <76864239+anirudhprabhakaran3@users.noreply.github.com>
2024-02-24 23:13:42 +0530

    Add admin skyward expedition (#128)
    * Add admin page and members page
    
    * Add team details page
    
    * Add Announcements CRUD
    
    * Send mails on announcement

--- corpus/skyward_expedition/urls.py
+++ corpus/skyward_expedition/urls.py
@@ -19,4 +19,38 @@ urlpatterns = [
         views.delete_invite,
         name="skyward_expedition_delete_invite",
     ),
+    path("admin/", views.admin, name="skyward_expedition_admin"),
+    path(
+        "admin/members/",
+        views.member_dashboard,
+        name="skyward_expedition_member_dashboard",
+    ),
+    path(
+        "admin/teams/", views.teams_dashboard, name="skyward_expedition_teams_dashboard"
+    ),
+    path(
+        "admin/teams/<int:team_id>",
+        views.team_details,
+        name="skyward_expedition_team_details",
+    ),
+    path(
+        "admin/announcements",
+        views.announcements_dashboard,
+        name="skyward_expedition_announcements_dashboard",
+    ),
+    path(
+        "admin/announcements/new/",
+        views.new_announcement,
+        name="skyward_expedition_new_announcement",
+    ),
+    path(
+        "admin/announcements/<int:announcement_id>/edit/",
+        views.edit_announcement,
+        name="skyward_expedition_edit_announcement",
+    ),
+    path(
+        "admin/announcements/<int:announcement_id>/delete/",
+        views.delete_announcement,
+        name="skyward_expedition_delete_announcement",
+    ),
 ]
