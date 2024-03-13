23b2f0416982f8dd62c403a683a1131afe343add
ady <adityapandia18@gmail.com>
2024-03-13 21:24:21 +0530

    Made pre-commit changes

--- corpus/pages/views.py
+++ corpus/pages/views.py
@@ -1,12 +1,15 @@
+from accounts.models import Core
+from accounts.models import ExecutiveMember
+from accounts.models import Faculty
 from config.models import SIG
 from config.models import Society
-from accounts.models import Core
-from accounts.models import Faculty
-from accounts.models import ExecutiveMember
+from django.db.models import Case
+from django.db.models import Q
+from django.db.models import Value
+from django.db.models import When
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
-from django.db.models import Q
-from django.db.models import Case, When, Value, CharField
+
 
 def index(request):
     # Get all societies to render on landing page Societies section
@@ -48,59 +51,8 @@ def sig(request, sig_name):
 
 
 def team(request):
-    members = ExecutiveMember.objects.filter(
-    Q(user__core__isnull=True) | Q(user__core=None)
-)
-    ieee_core_posts = [
-        "Convenor", 
-        "Chairperson", 
-        "Vice Chairperson",
-        "Secretary",
-        "Joint Secretary",
-        "Treasurer(Branch)",
-        "Treasurer(Institute)",
-        "Webmaster",
-        "Media Lead",
-        "Outreach Lead",
-        "Envision Lead",
-        "Labs Lead",  
-        ]
-    
-
-    compsoc_core_posts = [
-        "CompSoc Chair",
-        "CompSoc Vice Chair",
-        "CompSoc Secretary",
-        "CompSoc Project Head",
-        "CompSoc Project Coordinator",
-        "CIS Chair",
-        "CIS Secretary",
-        "CIS Project Head",
-
-    ]
-
-    diode_core_posts = [
-        "Diode Chair",
-        "SPS Chair",
-        "SPS Vice Chair",
-        "SPS Secretary",
-        "CAS Chair",
-        "CAS Vice Chair",
-        "CAS Secretary",
-        "RAS Chair",
-        "RAS Secretary",
-    ]
-
-    piston_core_posts = [
-        "Piston Chair",
-        "Piston Vice Chair",
-        "Piston Secretary",
-        "Piston Project Head",
-        "IAS Chair",
-        "IAS Secretary",
-    ]
-
-    ieee_core = Core.objects.filter(post__in=ieee_core_posts).order_by(
+    members = ExecutiveMember.objects.filter(Q(core__isnull=True) | Q(core=None))
+    ieee_core = Core.objects.filter(sig__name="ExeCom").order_by(
         Case(
             When(post="Convenor", then=Value(1)),
             When(post="Chairperson", then=Value(2)),
@@ -118,54 +70,18 @@ def team(request):
         )
     )
 
-    compsoc_core = Core.objects.filter(post__in=compsoc_core_posts).order_by(
-        Case(
-            When(post="CompSoc Chair", then=Value(1)),
-            When(post="CompSoc Vice Chair", then=Value(2)),
-            When(post="CompSoc Secretary", then=Value(3)),
-            When(post="CompSoc Project Head", then=Value(4)),
-            When(post="CompSoc Project Coordinator", then=Value(5)),
-            When(post="CIS Chair", then=Value(6)),
-            When(post="CIS Secretary", then=Value(7)),
-            When(post="CIS Project Head", then=Value(8)),
-            When(post="Webmaster", then=Value(9)),
-        )
-    )
-
-    diode_core = Core.objects.filter(post__in=diode_core_posts).order_by(
-        Case(
-            When(post="Diode Chair", then=Value(1)),
-            When(post="SPS Chair", then=Value(2)),
-            When(post="SPS Vice Chair", then=Value(3)),
-            When(post="SPS Secretary", then=Value(4)),
-            When(post="CAS Chair", then=Value(5)),
-            When(post="CAS Vice Chair", then=Value(6)),
-            When(post="CAS Secretary", then=Value(7)),
-            When(post="RAS Chair", then=Value(8)),
-            When(post="RAS Secretary", then=Value(9)),
-        )
-    )
-
-    piston_core = Core.objects.filter(post__in=piston_core_posts).order_by(
-        Case(
-            When(post="Piston Chair", then=Value(1)),
-            When(post="Piston Vice Chair", then=Value(2)),
-            When(post="Piston Secretary", then=Value(3)),
-            When(post="Piston Project Head", then=Value(4)),
-            When(post="IAS Chair", then=Value(5)),
-            When(post="IAS Secretary", then=Value(6)),
-        )
-    ) 
-    
+    compsoc_core = Core.objects.filter(sig__name="CompSoc").order_by("post")
+    diode_core = Core.objects.filter(sig__name="Diode").order_by("post")
+    piston_core = Core.objects.filter(sig__name="Piston").order_by("post")
 
     faculty = Faculty.objects.all()
     context = {
-        "members":members,
+        "members": members,
         "ieee_core": ieee_core,
-        "compsoc_core":compsoc_core,
-        "diode_core":diode_core,
-        "piston_core":piston_core,
-        "faculty":faculty,
+        "compsoc_core": compsoc_core,
+        "diode_core": diode_core,
+        "piston_core": piston_core,
+        "faculty": faculty,
     }
     return render(request, "pages/team.html", context)
 
