59db559c2d60cbc548673e990ffdd9b5e40bf0e5
Anirudh Prabhakaran <76864239+anirudhprabhakaran3@users.noreply.github.com>
2024-03-15 11:33:02 +0530

    Add Reset Password Feature (#138)
    * Add Reset Password Feature
    
    * Remove unnecessary code
    
    * Update some mail configurations
    
    * Added reset time to be 1 day

--- corpus/accounts/urls.py
+++ corpus/accounts/urls.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.views import PasswordResetCompleteView
+from django.contrib.auth.views import PasswordResetConfirmView
+from django.contrib.auth.views import PasswordResetDoneView
+from django.contrib.auth.views import PasswordResetView
 from django.urls import path
 
 from .views import signin
@@ -8,4 +12,16 @@ urlpatterns = [
     path("signup/", signup, name="accounts_signup"),
     path("login/", signin, name="accounts_signin"),
     path("logout/", signout, name="accounts_signout"),
+    path("reset/", PasswordResetView.as_view(), name="password_reset"),
+    path("reset/done/", PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path(
+        "reset/confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/complete/",
+        PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
