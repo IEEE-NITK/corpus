53f1c345e60fb89f37a86148e738a92f5c655c54
ady <adityapandia18@gmail.com>
2024-03-22 23:03:32 +0530

    Minor change in path string for teampage

--- corpus/pages/urls.py
+++ corpus/pages/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("about_us/", views.about_us, name="about_us"),
     path("sig/<str:sig_name>/", views.sig, name="sig"),
-    path("team", views.team, name="team"),
+    path("team/", views.team, name="team"),
 ]
