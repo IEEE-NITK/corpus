b8fed3d29e478333e9053ce7a865cf6b3d913876
Vignaraj Pai <vignarajpai@outlook.com>
2024-08-09 22:04:15 +0530

    Refactor navbar - v0 (#162)

--- corpus/templates/static/js/index.js
+++ corpus/templates/static/js/index.js
@@ -1,13 +1,52 @@
-$(document).ready(() => {
-    new TypeIt("#typewriter", {
-        loop: true,
-        deleteSpeed: 100
-    })
-        .type("Solving Problems", { delay: 1000 })
-        .delete()
-        .type("NITK", { delay: 1500 })
-        .delete()
-        .type("Humanity", { delay: 1500 })
-        .delete()
-        .go();
-});
+class TxtType {
+    constructor(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.isDeleting = false;
+        this.tick();
+    }
+
+    tick() {
+        const i = this.loopNum % this.toRotate.length;
+        const fullTxt = this.toRotate[i];
+        
+        this.txt = this.isDeleting ? 
+                   fullTxt.substring(0, this.txt.length - 1) : 
+                   fullTxt.substring(0, this.txt.length + 1);
+        
+        this.el.innerHTML = `<span class="wrap">${this.txt}</span>`;
+        
+        let delta = 200 - Math.random() * 100;
+        if (this.isDeleting) delta /= 2;
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(() => this.tick(), delta);
+    }
+}
+
+window.onload = () => {
+    document.querySelectorAll('.typewrite').forEach(el => {
+        const toRotate = JSON.parse(el.getAttribute('data-type'));
+        const period = el.getAttribute('data-period');
+        if (toRotate) new TxtType(el, toRotate, period);
+    });
+
+    // Inject CSS for typewriter effect
+    const css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+// Credits @ Simon Shahriveri for the typewriter effect https://codepen.io/hi-im-si
