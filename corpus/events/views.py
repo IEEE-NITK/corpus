344eda34ec62ea6f48bdf14898867b7cae11186c
yukitya-1811 <adityapandia18@gmail.com>
2024-10-21 17:40:26 +0530

    Make migrations, set up views and make the form

--- corpus/events/views.py
+++ corpus/events/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
+from django.contrib import messages
 # Create your views here.
 
 def dashboard(request):
-    return render(request, "events/dashboard.html")
+    events = Event.objects.all()
+    context = {
+        "events": events
+    }
+    return render(request, "events/dashboard.html", context)
 
 def create_event(request):
-    return render(request, "events/create_event.html")
+    if request.method == "POST":
+        event_form = EventForm(request.POST)
+        if event_form.is_valid():
+            event_form.save()
+            messages.success(request, ("New event created!"))
+        else:
+            print(event_form.errors)
+            # messages.error(request, ("Error creating event. Please try again."))
+        return redirect("dashboard")
+    
+    event_form = EventForm()
+    context = {
+        "event_form": event_form
+    }
+    return render(request, "events/create_event.html", context)
 
 def manage_event(request):
     return render(request, "events/manage_event.html")
