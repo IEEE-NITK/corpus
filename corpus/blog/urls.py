7eecfe5951943efb2891a9a90590aa63c7baff14
yukitya-1811 <adityapandia18@gmail.com>
2024-11-28 12:18:36 +0530

    Change media files location

--- corpus/blog/urls.py
+++ corpus/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -7,3 +8,6 @@ urlpatterns = [
     path("<slug:slug>", views.full_post, name="full_post"),
     path("tags/<int:specific_tag>", views.tagged_blog, name="tagged_blog"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
