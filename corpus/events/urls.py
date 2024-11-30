9a67817414f6efc1026ff9dd0ecaf367d4012e3e
yukitya-1811 <adityapandia18@gmail.com>
2024-11-30 11:36:39 +0530

    Frontend fixes and changes

--- corpus/events/urls.py
+++ corpus/events/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
+    path("", views.dashboard, name="events_dashboard"),
     path("core_dashboard", views.core_dashboard, name="events_core_dashboard"),
     path("new", views.new, name="new"),
     path("manage_event/<int:pk>", views.manage_event, name="manage_event"),
     path("delete_event/<int:pk>", views.delete_event, name="delete_event"),
+    path("show_event/<int:pk>", views.show_event, name="show_event"),
     path("report/<int:pk>", views.report, name="report"),
 ]
 
