c50b412788c9ddeccfdd4bbd81049feff0c571f4
yukitya-1811 <adityapandia18@gmail.com>
2024-12-04 01:27:11 +0530

    Allow select2 for easier selecting. Minor frontend changes

--- corpus/events/forms.py
+++ corpus/events/forms.py
@@ -3,6 +3,18 @@ from corpus.forms import CorpusModelForm
 from .models import Event
 
 class EventForm(CorpusModelForm):
+    required_css_class = 'required-field'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add an asterisk (*) to the labels of required fields
+        for field_name, field in self.fields.items():
+            if field.required:
+                if field.label:
+                    field.label = f"{field.label} *"
+                else:
+                    field.label = "*"
+
     class Meta:
         model = Event
         fields = "__all__"
