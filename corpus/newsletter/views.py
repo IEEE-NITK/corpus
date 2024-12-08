0c167307162af032b3d548bf4d9e96077edb5e7f
yukitya-1811 <adityapandia18@gmail.com>
2024-12-08 16:47:46 +0530

    Add upcoming events and completed events

--- corpus/newsletter/views.py
+++ corpus/newsletter/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from .models import NewsPost
+from events.models import Event
+from datetime import datetime
 
 # Create your views here.
 
 def home(request):
-    posts = NewsPost.objects.all()
-    context = {'posts': posts}
+    upcoming_events = Event.objects.filter(start_time__gte=datetime.now())
+    upcoming_events_posts = NewsPost.objects.filter(event__in=upcoming_events)
+    posts = NewsPost.objects.all().order_by('-date')[:5]
+    completed_events = Event.objects.filter(end_time__lte=datetime.now())
+    completed_events_posts = NewsPost.objects.filter(event__in=completed_events)
+    context = {
+        'upcoming_events_posts': upcoming_events_posts,
+        'completed_events_posts': completed_events_posts,
+        'posts': posts
+    }
     return render(request, "newsletter/home.html", context)
