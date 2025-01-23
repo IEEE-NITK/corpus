8c31cf5523e937abcf978469cd045953fb37c481
CodePokeX <sonta445@gmail.com>
2025-01-23 23:34:30 +0530

    Forbidden message changed

--- corpus/accounts/validators.py
+++ corpus/accounts/validators.py
@@ -48,9 +48,3 @@ def validate_ieee_email(value):
         raise ValidationError("Email must end with @ieee.org")
     email_validator(value)
 
-def validate_image(image):
-    file_size = image.file.size
-    limit = 5 * 1024 * 1024  # 5 MB limit
-    if file_size > limit:
-        raise ValidationError("Image size exceeds 5MB.")
-
