69fc998de1635748206753ab3c007b8d3cfd1fc5
alphaplayerofdooms <154459496+alphaplayerofdooms@users.noreply.github.com>
2025-01-27 10:39:38 +0530

    Return 404 in case no blog existed

--- corpus/blog/views.py
+++ corpus/blog/views.py
@@ -1,12 +1,10 @@
 
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django.shortcuts import redirect,render,get_object_or_404
 from django.template import Context
 from django.template import Template
 from django.core.paginator import Paginator
-from django.shortcuts import render
 from django.utils import timezone
 
 from .models import Post
@@ -30,7 +28,8 @@ def post_list(request):
 
 
 def full_post(request, slug):
-    individual_post = Post.objects.filter(published_date__lte=timezone.now()).get(
+    individual_post = get_object_or_404(
+        Post.objects.filter(published_date__lte=timezone.now()),
         slug=slug
     )
     return render(request, "blog/full_post.html", {"individual_post": individual_post})
@@ -38,10 +37,10 @@ def full_post(request, slug):
 
 # view for blogs filtered on the basis of tags
 def tagged_blog(request, specific_tag):
+    required_tag = get_object_or_404(Tag, id=specific_tag)
     specific_tag_blogs = Post.objects.filter(
         blog_tag=specific_tag, published_date__lte=timezone.now()
     ).order_by("-published_date")
-    required_tag = Tag.objects.get(id=specific_tag)
     paginator = Paginator(specific_tag_blogs, 9)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -53,6 +52,7 @@ def tagged_blog(request, specific_tag):
             "specific_tag_blogs": specific_tag_blogs,
             "required_tag": required_tag,
             "page_obj": page_obj,
-            "tags":tags,
+            "tags": tags,
         },
     )
+
