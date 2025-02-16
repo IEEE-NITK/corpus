fe79957d834e17b43b3506c380acf5ed594ef240
yukitya-1811 <adityapandia18@gmail.com>
2025-02-17 02:01:52 +0530

    Add models to newsletter module

--- corpus/newsletter/views.py
+++ corpus/newsletter/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from corpus.decorators import module_enabled
+from .models import *
 # Create your views here.
 
 @module_enabled(module_name="newsletter")
 def home(request):
-    return render(request, 'newsletter/home.html')
+    announcements = Announcement.objects.filter(archived=False).order_by('-created_at')
+    events = Event.objects.filter(archived=False).order_by('-created_at')
+    context = {
+        'announcements': announcements,
+        'events': events
+    }
+    return render(request, 'newsletter/home.html', context)
