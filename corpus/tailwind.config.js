7f0e30e891efbdcd8b1bfb1e6a7d55734d03b1e6
20Varun23 <varun.acharyakv2005@gmail.com>
2025-08-16 18:01:34 +0530

    Codered page

--- corpus/tailwind.config.js
+++ corpus/tailwind.config.js
@@ -16,7 +16,27 @@ module.exports = {
         require("daisyui"),
     ],
     daisyui: {
-        themes: ["winter", "night", "synthwave"],
+        themes: [
+            "winter",
+            "night",
+            "synthwave",
+            {
+                codeRed: {
+                    "primary": "#C3060A",
+                    "secondary": "#002357",
+                    "accent": "#011533",
+                    "neutral": "#011533",
+                    "base-100": "#330101",  // background
+                    "base-200": "#400102",  // cards
+                    "base-300": "#0d013b",  // borders
+                    "info": "#002357",
+                    "success": "#002357",
+                    "warning": "#EC0300",
+                    "error": "#EC0300",
+                }
+            }
+        ],
     },
-    darkMode: ['class', '[data-theme="night"]'],
+    darkMode: ['class', '[data-theme="night"]', '[data-theme="codeRed"]'],
 }
+
