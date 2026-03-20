118ff426cea05978f157308f13391c0bb3d5ab0f
Rajsimha M V <rajsimhamv2006@gmail.com>
2026-03-20 08:37:49 +0530

    Landing page added

--- corpus/tlm/views.py
+++ corpus/tlm/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 def home(request):
     return render(request, "tlm/home.html")
+
+def landing(request):
+    return render(request, 'tlm/landing.html')
